645146d3e989b7aedd39b72e5f7194d9a381f1bd
john ndichu <johnndichu104@gmail.com>
2024-11-28 15:38:47 +0300

    test first template of index.html

--- adventureApp/urls.py
+++ adventureApp/urls.py
@@ -1,8 +1,5 @@
-
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('adventureApp.urls')),
+    path('', views.index, name='index'),
 ]
